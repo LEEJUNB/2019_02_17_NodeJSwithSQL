bac8b58ecc50f60785d97bcf4ab951824c545be6
junblee <topguide82@gmail.com>
2019-02-10 23:47:51 +0900

    repeat

--- database_msql2.js
+++
@@ -1,7 +0,0 @@
-var mysql = require('mysql'); // node-mysql module
-var connection = mysql.createConnection({
-    host : 'localhost',
-    user : 'root',
-    password : '000000',
-    database : 'o2'
-}); //
